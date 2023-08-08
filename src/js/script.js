600042acf5c419098c69da11535ba2938db7dbdd
michi <michikotakahashi0129@gmail.com>
2023-08-08 11:03:12 +0900

    0808

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,16 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+        //ハンバーガーメニュー
+        //クリックするとクラスを付与
+        $(".js-hamburger").click(function(){
+        if ($('.js-hamburger').hasClass('is-active') ) {
+            $('.js-hamburger').removeClass('is-active');
+            $(".sp-nav").removeClass('is-active');
+        } else {
+            $('.js-hamburger').addClass('is-active');
+            $(".sp-nav").addClass('is-active');
+        }
+        });
 
 });
