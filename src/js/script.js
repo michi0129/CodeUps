81ea9093b80b6b996c49269056ec4a2bcdffc4a5
michi <michikotakahashi0129@gmail.com>
2023-08-25 07:08:31 +0900

    0825

--- src/js/script.js
+++ src/js/script.js
@@ -148,3 +148,17 @@ $(this).delay(200).animate({'width':'100%'},speed,function(){
         }
     });
 });
+
+var topBtn = $('.to-top');
+  topBtn.hide();
+
+  // ボタンの表示設定
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 70) {
+      // 指定px以上のスクロールでボタンを表示
+      topBtn.fadeIn();
+    } else {
+      // 画面が指定pxより上ならボタンを非表示
+      topBtn.fadeOut();
+    }
+  });
